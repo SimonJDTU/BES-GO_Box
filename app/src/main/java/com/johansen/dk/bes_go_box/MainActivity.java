fef21b5f27b7fc4fac1fe4c5019c7750560da6f6
Simon J <Neelix548@hotmail.com>
2019-11-19 23:20:42 +0100

    Initial commit

---
+++ app/src/main/java/com/johansen/dk/bes_go_box/MainActivity.java
@@ -0,0 +1,14 @@
+package com.johansen.dk.bes_go_box;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
