8fe31936073c0c4c01b945328e068b03c2063514
SimonJDTU <SimonJDTU@gmail.com>
2019-11-20 14:37:00 +0100

    Implemented firebase Auth and removed QR-scanner from mainActivity

--- app/src/main/java/com/johansen/dk/bes_go_box/Activities/MainActivity.java
+++ app/src/main/java/com/johansen/dk/bes_go_box/Activities/MainActivity.java
@@ -1,27 +1,21 @@
 package com.johansen.dk.bes_go_box.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Vibrator;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.johansen.dk.bes_go_box.R;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -29,103 +23,40 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.io.IOException;
-import java.util.regex.Pattern;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-import static android.os.Build.VERSION_CODES.LOLLIPOP;
-
-public class MainActivity extends AppCompatActivity {
-
-    private SurfaceView cameraPreview;
-    private CameraSource cameraSrc;
-    private BarcodeDetector barcodeDetector;
-    private TextView loginInfo;
-    private Vibrator vibe;
-
-    private long scanTime = 0;
+    private FirebaseAuth mAuth;
+    private TextView eMail,password;
+    private Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginInfo = findViewById(R.id.informationText);
+        mAuth = FirebaseAuth.getInstance();
 
-        cameraPreview = findViewById(R.id.cameraPreview);
-        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
-        cameraSrc = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(1024, 768)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedFps(30.0f)
-                .setAutoFocusEnabled(true)
-                .build();
+        eMail = (TextView) findViewById(R.id.main_mail);
+        password = (TextView) findViewById(R.id.main_password);
 
-        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        login = (Button) findViewById(R.id.main_login);
+        login.setOnClickListener(this);
+        register = (Button) findViewById(R.id.main_register);
+        register.setOnClickListener(this);
+
     }
 
-
-
-    private void createQRscan() {
-        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                if(!(Build.VERSION.SDK_INT <= LOLLIPOP)) {
-                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        askPermission();
-                        return;
-                    }
-                }
-                try {
-                    cameraSrc.start(holder);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSrc.stop();
-            }
-        });
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-
-                if (qrCodes.size() != 0) {
-                    loginInfo.post(() -> {
-                        if (timeAllowedBetweenScans()) {
-                            scanTime = System.currentTimeMillis();
-                            if (true/*qrRegex(qrCodes.valueAt(0).displayValue)*/) {
-
-                                vibe.vibrate(200);
-                                //TODO: Refractor activityname "nextActivity" to something more appropriate
-                                Intent intent = new Intent(MainActivity.this, NextActivity.class);
-                                intent.putExtra("QRvalue", qrCodes.valueAt(0).displayValue);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(getApplicationContext(),"Couldn't recognize QR-code, try again", Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-                    });
-                }
-            }
-        });
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
-    private boolean timeAllowedBetweenScans() {
-        long currentTime = System.currentTimeMillis();
-        return currentTime - scanTime > 3000;
-    }
+    private void updateUI(FirebaseUser currentUser) {
 
+    }
 
     // Must be done during an initialization phase like onCreate
     private void askPermission() {
@@ -154,31 +85,39 @@ public class MainActivity extends AppCompatActivity {
                 }).check();
     }
 
-    private boolean qrRegex(String codeString) {
-
-        // 6-14 character password requiring numbers and alphabets of both cases
-        final String PASSWORD_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{16}$";
-
-        /* 4-32 character password requiring at least 3 out of 4 (uppercase
-        // and lowercase letters, numbers & special characters) and at-most
-        // 2 equal consecutive chars.
-        final String COMPLEX_PASSWORD_REGEX =
-                "^(?:(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])|" +
-                        "(?=.*\\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|" +
-                        "(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|" +
-                        "(?=.*\\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))(?!.*(.)\\1{2,})" +
-                        "[A-Za-z0-9!~<>,;:_=?*+#.\"&§%°()\\|\\[\\]\\-\\$\\^\\@\\/]" +
-                        "{8,32}$";
-        */
-
-        final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
-
-        return PASSWORD_PATTERN.matcher(codeString).matches();
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.main_login:
+                    signInUser();
+                break;
+            case R.id.main_register:
+                break;
+                default:
+                    Toast.makeText(this,"DEFAULT HIT IN ONCLICK SWITCH",Toast.LENGTH_SHORT).show();
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        createQRscan();
+    private void signInUser() {
+        mAuth.signInWithEmailAndPassword(eMail.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(getBaseContext(), "SUCCESS",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getBaseContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
